21e596aad6a1f77086e63f5eb490b066406333a7
영철 신 <youngchul.shin@meshkorea.net>
2020-03-24 18:25:57 +0900

    JPA 2장

--- ch2/src/main/java/com/my/jpabook/JpabookApplication.java
+++ ch2/src/main/java/com/my/jpabook/JpabookApplication.java
@@ -1,12 +1,18 @@
 package com.my.jpabook;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class JpabookApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpabookApplication.class, args);
 	}
 
